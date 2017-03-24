8334b9836797f53e5fe1ade48f55aed42b3e6b23
admin <admin@TECLAST>
2017-03-24 12:08:39 +0100

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,263 @@
+#define GLEW_STATIC
+#include <GL\glew.h>
+//GLFW
+#include <GLFW\glfw3.h>
+#include <iostream>
+#include "..\Shader.h"
+#include <SOIL.h>
+
+using namespace std;
+const GLint WIDTH = 800, HEIGHT = 600;
+bool WIDEFRAME = false;
+bool paintQuad=false;
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+float mCoef = 0;
+
+
+void DrawVao(GLuint programID,GLuint VAO) {
+	//establecer el shader
+	glUseProgram(programID);
+
+	//pitar el VAO
+	glBindVertexArray(VAO);
+	if (!paintQuad) {
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	}
+	else {
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	}
+
+}
+int main() {
+	//initGLFW
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
+//TODO
+
+	//set GLFW
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+
+	//create a window
+//TODO
+	GLFWwindow* window;
+
+
+
+	window = glfwCreateWindow(WIDTH, HEIGHT, "Primera ventana", nullptr, nullptr);
+	if (!window) {
+		cout << "Error al crear la ventana" << endl;
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+	glfwMakeContextCurrent(window);
+
+	//set GLEW and inicializate
+//TODO
+
+	glewExperimental = GL_TRUE;
+	if (GLEW_OK != glewInit()) {
+		std::cout << "Error al inicializar glew" << std::endl;
+		glfwTerminate();
+		return NULL;
+	}
+	int screenWithd, screenHeight;
+	glfwGetFramebufferSize(window, &screenWithd, &screenHeight);
+	//set function when callback
+//TODO
+		glfwSetKeyCallback(window, key_callback);
+
+	//set windows and viewport
+//TODO
+		glViewport(0, 0, screenWithd, screenHeight);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+	//fondo
+		glClearColor(0.5f, 0.5f, 0.5f, 1.0);
+
+
+//TODO
+
+
+	//cargamos los shader
+	Shader s = Shader("./src/textureVertex.vertexshader", "./src/textureFragment.fragmentshader");
+
+	// Definir el buffer de vertices
+	//Reserva de memoria
+
+	GLfloat Vertices[] = {
+	0.5f,0.5f,0,	1.0f,.0f,.0f,	1.f,1.f,
+	0.5f,-0.5f,0,	0.f, 1.f, 1.f,	1.f, 0.f,
+	-0.5f,-0.5f,0,	0.f, 0.f, 1.f,	0.f, 0.f,
+	-0.5f,0.5f,0,	1.f, 1.f, 0.f,	0.f, 1.f
+	};
+
+	GLuint IndexBufferObject[]{
+		2,0,3,
+		2,1,0 };
+
+	GLuint VAO, EBO, VBO;
+	glGenVertexArrays(1,&VAO);
+	glBindVertexArray(VAO); {
+
+		glGenBuffers(1, &VBO);
+		//Se enlaza el buffer para poder usarlo
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		//Se pasan los datos
+		glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_DYNAMIC_DRAW);
+
+
+		glGenBuffers(1, &EBO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(IndexBufferObject), IndexBufferObject, GL_DYNAMIC_DRAW);
+		
+
+		//Propiedades
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)0);
+		glEnableVertexAttribArray(0);
+
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GL_FLOAT)));
+		glEnableVertexAttribArray(1);
+
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)(6* sizeof(GL_FLOAT)));
+		glEnableVertexAttribArray(2);
+
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	}glBindVertexArray(0);
+
+
+
+	//reservar memoria para el VAO, VBO y EBO&
+
+
+
+	//Establecer el objeto
+		//Declarar el VBO y el EBO
+
+		//Enlazar el buffer con openGL
+
+		//Establecer las propiedades de los vertices
+
+		//liberar el buffer
+	
+	//liberar el buffer de vertices
+	
+
+	/*unsigned char* tex2 = SOIL_load_image("texture2.png", &width, &height, 0, SOIL_LOAD_RGB);
+	SOIL_free_image_data(tex2);*/
+
+	//CREAR TEXTURAS
+	GLuint texture1, texture2;
+	int width, height;
+	
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	/*glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);*/
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	unsigned char* tex1 = SOIL_load_image("./src/texture.png", &width, &height, 0, SOIL_LOAD_RGB);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, tex1);
+	SOIL_free_image_data(tex1);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	//TEXTURA 2
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	/*glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);*/
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	unsigned char* tex2 = SOIL_load_image("./src/texture2.png", &width, &height, 0, SOIL_LOAD_RGB);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, tex2);
+	SOIL_free_image_data(tex2);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	
+
+	GLint mixCoef;
+	
+	//bucle de dibujado
+	while (!glfwWindowShouldClose(window))
+	{
+		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+		glfwPollEvents();
+		//Establecer el color de fondo
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		mixCoef = glGetUniformLocation(s.Program, "mCoef");
+		glUniform1f(mixCoef, mCoef);
+		std::cout << mCoef << std::endl;
+		/*DrawVao(s.Program, VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);*/
+
+		//pintar con lineas
+		//pintar con triangulos
+		/*glBindTexture(GL_TEXTURE_2D, texture1);
+		s.USE();
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);*/
+
+		s.USE();
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glUniform1i(glGetUniformLocation(s.Program, "ourTexture"), 0);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
+		glUniform1i(glGetUniformLocation(s.Program, "ourTexture2"), 1);
+
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+
+		// Swap the screen buffers
+		glfwSwapBuffers(window);
+	}
+	// liberar la memoria de los VAO, EBO y VBO
+	glBindTexture(GL_TEXTURE_2D,0);
+	// Terminate GLFW, clearing any resources allocated by GLFW.
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	exit(EXIT_SUCCESS);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+	//TODO, comprobar que la tecla pulsada es escape para cerrar la aplicación y la tecla w para cambiar a modo widwframe
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+
+	if (key == GLFW_KEY_W && action == GLFW_PRESS) {
+		paintQuad = !paintQuad;
+	}
+
+	if ((key == GLFW_KEY_UP || key == GLFW_KEY_W) && action == GLFW_PRESS) {
+		
+		if (mCoef < 1.f) {
+			mCoef += 0.1f;
+		}
+	}
+	else if ((key == GLFW_KEY_DOWN || key == GLFW_KEY_S) && action == GLFW_PRESS) {
+		
+		if (mCoef > 0.1f) {
+			mCoef -= 0.1f;
+		}
+	}
+}
