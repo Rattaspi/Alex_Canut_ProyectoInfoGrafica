bbc5c8c958a3064f00eb1a51370e4efe6aeb1240
Rattaspi <admin@TECLAST>
2017-04-22 20:54:14 +0200

    Creando la funcion que genera las matrices modelo para reutilizar los VBO.

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@ const GLint WIDTH = 800, HEIGHT = 600;
 bool WIDEFRAME = false;
 bool paintQuad=false;
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+glm::mat4 modelMatGen(glm::vec3 scale, glm::vec3 rotate, glm::vec3 translate);
 float mCoef = 0;
 float deg = 0;
 bool l, r;
@@ -83,16 +84,62 @@ int main() {
 
 	// Definir el buffer de vertices
 	//Reserva de memoria
-	GLfloat Vertices[] = {
-	0.5f,0.5f,0,	1.0f,.0f,.0f,	1.f,1.f,
-	0.5f,-0.5f,0,	0.f, 1.f, 1.f,	1.f, 0.f,
-	-0.5f,-0.5f,0,	0.f, 0.f, 1.f,	0.f, 0.f,
-	-0.5f,0.5f,0,	1.f, 1.f, 0.f,	0.f, 1.f
+	GLfloat VertexBufferCube[] = {
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		0.5f , -0.5f, -0.5f,  1.0f, 0.0f,
+		0.5f ,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f ,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		0.5f , -0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f ,  0.5f,  0.5f,  1.0f, 1.0f,
+		0.5f ,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		0.5f ,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f ,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f , -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f , -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f , -0.5f,  0.5f,  0.0f, 0.0f,
+		0.5f ,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f , -0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f , -0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f , -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f ,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f ,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f ,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
 
-	GLuint IndexBufferObject[]{
-		2,0,3,
-		2,1,0 };
+	glm::vec3 CubesPositionBuffer[] = {
+		glm::vec3(0.0f ,  0.0f,  0.0f),
+		glm::vec3(2.0f ,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f , -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f , -2.0f, -2.5f),
+		glm::vec3(1.5f ,  2.0f, -2.5f),
+		glm::vec3(1.5f ,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
 
 	GLuint VAO, EBO, VBO;
 	glGenVertexArrays(1,&VAO);
@@ -102,24 +149,14 @@ int main() {
 		//Se enlaza el buffer para poder usarlo
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		//Se pasan los datos
-		glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_DYNAMIC_DRAW);
-
-
-		glGenBuffers(1, &EBO);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(IndexBufferObject), IndexBufferObject, GL_DYNAMIC_DRAW);
-		
+		glBufferData(GL_ARRAY_BUFFER, sizeof(VertexBufferCube), VertexBufferCube, GL_DYNAMIC_DRAW);
 
 		//Propiedades
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (GLvoid*)0);
 		glEnableVertexAttribArray(0);
 
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GL_FLOAT)));
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GL_FLOAT)));
 		glEnableVertexAttribArray(1);
-
-		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)(6* sizeof(GL_FLOAT)));
-		glEnableVertexAttribArray(2);
-
 
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 
@@ -181,14 +218,15 @@ int main() {
 
 		//calculo matriz modelo
 		modelMat = glm::translate(modelMat, glm::vec3(0.f, -0.5f, 0.f));
-		modelMat = glm::rotate(modelMat, glm::radians(50.f), glm::vec3(1, 0, 0));
+		modelMat = glm::rotate(modelMat, glm::radians(50.f), glm::vec3(1, 0, 1));
 		
 		//calculo matriz vista
-		viewMat = glm::translate(viewMat, glm::vec3(0.f, 0.f, -0.3f));
+		viewMat = glm::translate(viewMat, glm::vec3(0.f, 0.f, 0.3f));
+		//viewMat = glm::rotate(viewMat, glm::radians(180.f), glm::vec3(0,1,0));
 		//UTILIZAR UN LOOKAT
 
 		//calculo matriz proyeccion
-		projectionMat = glm::perspective(glm::radians(FOV), (GLfloat)(WIDTH/HEIGHT), 0.1f, 100.f);
+		//projectionMat = glm::perspective(glm::radians(FOV), (GLfloat)(WIDTH/HEIGHT), 0.1f, 100.f);
 
 		finalMat = modelMat * viewMat * projectionMat/* * viewMat * modelMat*/;
 
@@ -254,3 +292,5 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 		l = false;
 	}
 }
+
+void ModelMatGen ()
