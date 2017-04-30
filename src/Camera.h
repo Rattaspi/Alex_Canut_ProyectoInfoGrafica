4e08c436d81f2899ed1c0ba4deb6fa4f5c3e0735
Rattaspi <admin@TECLAST>
2017-04-30 22:52:28 +0200

    Mas cosas del header de la camara.

--- src/Camera.h
+++ src/Camera.h
@@ -25,3 +25,17 @@ public:
 	glm::mat4 LookAt();
 	GLfloat GetFOV();
 };
+
+Camera::Camera(glm::vec3 position, glm::vec3 direction, GLfloat sensivility, GLfloat fov) {
+	lastFrame = 0;
+	cameraPos = position;
+	cameraFront = direction;
+	this->sensivility = sensivility;
+	FOV = fov;
+
+	glm::vec3 directionX = glm::normalize(glm::vec3(0, direction.y, direction.z));
+	glm::vec3 z(0, 0, 1);
+	glm::vec3 directionY = glm::normalize(glm::vec3(direction.x, 0, direction.z));
+	PITCH = 90 - glm::degrees(glm::acos(glm::dot(glm::vec3(0, 1, 0), directionX)));
+	YAW = 90 - glm::degrees(glm::acos(glm::dot(directionY, z)));
+}
