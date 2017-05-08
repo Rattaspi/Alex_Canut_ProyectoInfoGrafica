343fe3d78e4c47094cbb22af144f8ab6ad26273c
Rattaspi <admin@TECLAST>
2017-05-08 18:18:38 +0200

    La ultima escena ya esta montada y las luces estan añadidas, parece que se ilumina la caja y todo

---
+++ src/FragmentShaderPhongTexture.fs
@@ -0,0 +1,123 @@
+#version 330 core
+
+#define NUM_MAX_PLIGHTS 2
+#define NUM_MAX_SLIGHTS 2
+
+struct Material {
+	sampler2D TexturaDifusa;
+	sampler2D TexturaEspecular;
+	float shininess;
+};  
+
+struct PLight {
+	vec3 luzAmbiental;
+	vec3 luzDifusa;
+	vec3 luzEspecular;
+	vec3 pos;
+};
+
+struct DLight {
+	vec3 luzAmbiental;
+	vec3 luzDifusa;
+	vec3 luzEspecular;
+	vec3 pos;
+	vec3 dir;
+};
+
+struct SLight {
+	vec3 luzAmbiental;
+	vec3 luzDifusa;
+	vec3 luzEspecular;
+	vec3 pos;
+	vec3 dir;
+	float aperturaMax;
+	float aperturaMin;
+};
+
+in vec3 FragPos;  
+in vec3 Normal;  
+in vec2 TexCoords;
+  
+out vec4 color;
+  
+uniform vec3 viewPos;
+uniform Material mat;
+uniform DLight dlight;
+uniform PLight plight[NUM_MAX_PLIGHTS];
+uniform SLight slight[NUM_MAX_SLIGHTS];
+
+vec3 DirectionalLight(DLight light, vec3 Normal, vec3 viewDirection);
+vec3 PointLight(PLight light, vec3 Normal, vec3 viewDirection);
+vec3 SpotLight(SLight light, vec3 Normal, vec3 viewDirection);
+
+void main(){
+	vec3 viewPortD = normalize(viewPos - FragPos);
+
+	color = vec4(DirectionalLight(dlight,Normal,viewPortD) + PointLight(plight[0], Normal, viewPortD) + SpotLight(slight[0], Normal, viewPortD), 1);
+} 
+
+vec3 DirectionalLight(DLight light, vec3 Normal, vec3 viewDirection){
+	vec3 finalColor;
+	vec3 ambient = vec3(texture(mat.TexturaDifusa, TexCoords)) * light.luzAmbiental;
+
+	vec3 incidenciaLuzNormal = normalize(-light.dir);
+	float diff = max(dot(normalize(Normal.xyz), incidenciaLuzNormal), 0.0);
+	vec3 R = reflect(-incidenciaLuzNormal, normalize(Normal.xyz));
+	float esp = pow(max(dot(R, viewDirection), 0.0), mat.shininess);
+	vec3 diffuse = light.luzDifusa * diff * vec3(texture(mat.TexturaEspecular, TexCoords));
+	vec3 specular = light.luzEspecular * esp * vec3(texture(mat.TexturaEspecular, TexCoords));
+
+	finalColor = ambient + diffuse + specular;
+	return finalColor;
+}
+
+vec3 PointLight(PLight light, vec3 Normal, vec3 viewDirection){
+	vec3 finalColor;
+	vec3 ambient = vec3(texture(mat.TexturaDifusa, TexCoords)) * light.luzAmbiental;
+
+	vec3 lightDir = normalize(light.pos - FragPos);
+	float diff = max(dot(normalize(Normal), lightDir), 0.0);
+	vec3 diffuse = light.luzDifusa * diff * vec3(texture(mat.TexturaDifusa, TexCoords));
+
+	vec3 R = reflect(-lightDir, normalize(Normal));
+	float esp = pow(max(dot(R, viewDirection), 0.0), mat.shininess);
+	vec3 specular = light.luzEspecular * esp * vec3(texture(mat.TexturaEspecular, TexCoords));
+
+	float d = length(light.pos - FragPos);
+	float c1, c2, c3;
+	c1 = 1.0f;
+	c2 = 0.045;
+	c3 = 0.0075f;
+	float factorAtenuacion = 1 / (1 + c2*d + c3*(d*d));
+
+	finalColor = factorAtenuacion * (ambient + diffuse + specular);
+	return finalColor;
+}
+
+vec3 SpotLight(SLight light, vec3 Normal, vec3 viewDirection){
+	vec3 finalColor;
+	vec3 ambient = vec3(texture(mat.TexturaDifusa, TexCoords)) * light.luzAmbiental;
+
+	vec3 lightDir = normalize(light.pos - FragPos);
+	float diff = max(dot(normalize(Normal), lightDir), 0.0);
+	vec3 diffuse = light.luzDifusa * diff * vec3(texture(mat.TexturaDifusa, TexCoords));
+
+	vec3 R = reflect(-lightDir, normalize(Normal));
+	float esp = pow(max(dot(R, viewDirection), 0.0), mat.shininess);
+	vec3 specular = light.luzEspecular * esp * vec3(texture(mat.TexturaEspecular, TexCoords));
+
+	float d = length(light.pos - FragPos);
+	float c1, c2, c3;
+	c1 = 1.0f;
+	c2 = 0.045;
+	c3 = 0.0075f;
+	float factorAtenuacion = 1 / (1 + c2*d + c3*(d*d));
+
+	vec3 incidenciaLuzNormal = normalize(light.pos - FragPos);
+	float theta = dot(incidenciaLuzNormal, -normalize(light.dir));
+	float epsilon = light.aperturaMin - light.aperturaMax;
+	float inte = clamp((theta - light.aperturaMax)/epsilon, 0, 1);
+
+	finalColor = inte * factorAtenuacion * (ambient + diffuse + specular);
+	return finalColor;
+}
---
+++ src/Light.cpp
@@ -0,0 +1,89 @@
+#include "Light.h"
+
+Light::Light(vec3 pos, vec3 dir, vec3 ambient, vec3 diffuse, vec3 specular, LType lType, int number) {
+	switch (lType) {
+	case DIRECTIONAL:
+		SetPosition(pos);
+		SetDirection(dir);
+		break;
+	case POINT:
+		SetPosition(pos);
+		break;
+	case SPOT:
+		SetPosition(pos);
+		SetDirection(dir);
+		break;
+	default:
+		break;
+	}
+
+	Lambient = ambient;
+	Ldiffuse = diffuse;
+	Lspecular = specular;
+	LightType = lType;
+	lightNumber = number;
+}
+
+
+void Light::SetAtt(float constant, float lineal, float quadratic) {
+	c1 = constant;
+	c2 = lineal;
+	c3 = quadratic;
+}
+
+void Light::SetAperture(float min, float max) {
+	MinAperture = min;
+	MaxAperture = max;
+}
+
+void Light::SetLight(Shader *shad, vec3 CamPos) {
+	std::string variable;
+
+	glUniform3f(glGetUniformLocation(shad->Program, "viewPos"), CamPos.x, CamPos.y, CamPos.z);
+	switch (LightType){
+	case DIRECTIONAL:
+		glUniform3f(glGetUniformLocation(shad->Program, "dlight.luzAmbiental"), Lambient.x, Lambient.y, Lambient.z);
+		glUniform3f(glGetUniformLocation(shad->Program, "dlight.luzDifusa"), Ldiffuse.x, Ldiffuse.y, Ldiffuse.z);
+		glUniform3f(glGetUniformLocation(shad->Program, "dlight.luzEspecular"), Lspecular.x, Lspecular.y, Lspecular.z);
+		glUniform3f(glGetUniformLocation(shad->Program, "dlight.pos"), Lpos.x, Lpos.y, Lpos.z);
+		glUniform3f(glGetUniformLocation(shad->Program, "dlight.dir"), Ldirection.x, Ldirection.y, Ldirection.z);
+		break;
+	case POINT:		
+		variable = "plight[" + std::to_string(lightNumber) + "]";		
+		glUniform3f(glGetUniformLocation(shad->Program, (variable + ".luzAmbiental").c_str()), Lambient.x, Lambient.y, Lambient.z);
+		glUniform3f(glGetUniformLocation(shad->Program, (variable + ".luzDifusa").c_str()), Ldiffuse.x, Ldiffuse.y, Ldiffuse.z);
+		glUniform3f(glGetUniformLocation(shad->Program, (variable + ".luzEspecular").c_str()), Lspecular.x, Lspecular.y, Lspecular.z);
+		glUniform3f(glGetUniformLocation(shad->Program, (variable + ".pos").c_str()), Lpos.x, Lpos.y, Lpos.z);
+		break;
+	case SPOT:
+		variable = "slight[" + std::to_string(lightNumber) + "]";
+		glUniform3f(glGetUniformLocation(shad->Program, (variable + ".luzAmbiental").c_str()), Lambient.x, Lambient.y, Lambient.z);
+		glUniform3f(glGetUniformLocation(shad->Program, (variable + ".luzDifusa").c_str()), Lpos.x, Lpos.y, Lpos.z);
+		glUniform3f(glGetUniformLocation(shad->Program, (variable + ".luzEspecular").c_str()), Lambient.x, Lambient.y, Lambient.z);
+		glUniform3f(glGetUniformLocation(shad->Program, (variable + ".pos").c_str()), Lambient.x, Lambient.y, Lambient.z);
+		glUniform3f(glGetUniformLocation(shad->Program, (variable + ".dir").c_str()), Lambient.x, Lambient.y, Lambient.z);
+		glUniform1f(glGetUniformLocation(shad->Program, (variable + ".aperturaMax").c_str()), Lambient.x);
+		glUniform1f(glGetUniformLocation(shad->Program, (variable + ".aperturaMin").c_str()), Lambient.x);
+		break;
+	default:
+		break;
+	}	
+}
+
+void Light::Rotate(vec3 rotation) {	
+//opcional
+}
+
+void Light::SetDirection(vec3 dir) {
+	Ldirection = dir;
+}
+
+void Light::SetPosition(vec3 pos) {
+	Lpos = pos;
+}
+
+vec3 Light::GetColor() {
+	return vec3(0);
+}
+
+Light::~Light() {}
