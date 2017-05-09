dec7ecf7bf0f8a8a1ee7b7f83343910a49528161
Rattaspi <admin@TECLAST>
2017-05-09 08:48:25 +0200

    cosas

---
+++ src/material.cpp
@@ -0,0 +1,20 @@
+#include "material.hpp"
+
+Material::Material(char *DiffPath, char*SpectPath, float Shini){
+
+}
+
+Material::~Material(){
+}
+
+void Material::SetMaterial(Shader *shad) {
+
+}
+
+void Material::SetShininess(Shader *shad) {
+
+}
+
+void Material::ActivateTextures() {
+
+}
